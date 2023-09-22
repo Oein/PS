d196769abf6b7d124be5e8cb2bb9b738a9f3bc25
Oein <oein0219@gmail.com>
2023-09-23 08:35:10 +0900

    solve: acmicpc / 18870.cpp

---
+++ acmicpc/188__/18870.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long ull;
+typedef pair<int, int> pii;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    vector<int> v;
+    vector<int> y;
+    map<int, int> g;
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        y.push_back(x);
+    }
+    sort(y.begin(), y.end());
+    int q = 1;
+    for(auto p : y) {
+        if(g.find(p) != g.end()) continue;
+        g[p] = q;
+        q++;
+    }
+    for(auto x : v) {
+        cout << g[x] - 1 << " ";
+    }
+}
