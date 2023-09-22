2c8764465f0127224f92b4f10a1d64d0c7ac91d7
Oein <oein0219@gmail.com>
2023-09-23 08:35:08 +0900

    solve: acmicpc / 11718.cpp

---
+++ acmicpc/117__/11718.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+    string str;
+    while (true)
+    {
+        getline(cin, str);
+        if (str=="")
+            break;
+        cout << str << endl;
+    }
+ 
+    return 0;
+}
