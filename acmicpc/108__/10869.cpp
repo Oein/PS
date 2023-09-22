dde5a3e4e9f18c4ad60e836096197ef7a74c8f35
Oein <oein0219@gmail.com>
2023-09-23 08:35:07 +0900

    solve: acmicpc / 10869.cpp

---
+++ acmicpc/108__/10869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // cout.precision(33);
+    int A;
+    int B;
+    cin>>A;
+    cin>>B;
+    cout<<A+B<<endl;
+    cout<<A-B<<endl;
+    cout<<A*B<<endl;
+    cout<<A/B<<endl;
+    cout<<A%B<<endl;
+    return 0;
+}
