9f718636877b7ac106fd8b034b1cff0243727fbe
Oein <oein0219@gmail.com>
2023-09-23 08:35:14 +0900

    solve: acmicpc / 27323.cpp

---
+++ acmicpc/273__/27323.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long ull;
+typedef pair<int, int> pii;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+const int dx[4] = {0,0,1,-1};
+const int dy[4] = {1,-1,0,0};
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int a,b;
+    cin >> a >> b;
+    cout << a * b;
+}
