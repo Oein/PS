12fdb9f8c53c60fa4bde6fde9fe0861113c3c054
Oein <oein0219@gmail.com>
2023-09-23 08:35:11 +0900

    solve: acmicpc / 2164.cpp

---
+++ acmicpc/21__/2164.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	cin >> n;
+
+	queue<int> que;
+
+	for (int i = 1; i <= n; i++) {
+		que.push(i);
+	}
+
+	while (que.size() != 1)
+	{
+		que.pop();
+
+		que.push(que.front());
+		que.pop();
+	}
+
+	cout << que.front();
+	return 0;
+}
