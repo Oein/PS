f9c14433ad8380bdad0781351dfaa9430d52da17
Oein <oein0219@gmail.com>
2023-09-23 08:35:07 +0900

    solve: acmicpc / 10951.cpp

---
+++ acmicpc/109__/10951.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+int main() {
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    int a = 0, b = 0;
+    while (cin >> a >> b)
+    {
+        cout << a + b << '\n';
+    }
+}
