f56d1cc79b3147533c95de164bb68cd8196b5108
Oein <oein0219@gmail.com>
2023-09-23 08:35:14 +0900

    solve: acmicpc / 2739.cpp

---
+++ acmicpc/27__/2739.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+int main() {
+    int n = 0;
+    cin >> n;
+    for (int i = 1; i < 10; i++) cout << n << " * " << i << " = " << n * i << '\n';
+}
