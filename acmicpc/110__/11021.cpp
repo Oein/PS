96440e21f8d94dc8b828f2bdfd527aea8c8f7f83
Oein <oein0219@gmail.com>
2023-09-23 08:35:07 +0900

    solve: acmicpc / 11021.cpp

---
+++ acmicpc/110__/11021.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+int main() {
+    int n = 0;
+    vector<int>as;
+    vector<int>bs;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a = 0, b = 0;
+        cin >> a >> b;
+        as.push_back(a);
+        bs.push_back(b);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Case #" << i + 1 << ": " << as[i] + bs[i] << '\n';
+    }
+}
