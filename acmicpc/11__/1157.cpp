d29414d0afa00ef6d4d4e96c8e46a552a8ab143b
Oein <oein0219@gmail.com>
2023-09-23 08:35:08 +0900

    solve: acmicpc / 1157.cpp

---
+++ acmicpc/11__/1157.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string c;
+    cin >> c;
+    int max = 0;
+    int cnt = 0;
+    int target = 0;
+    int a[26] = {0,};
+    transform(c.begin(), c.end(), c.begin(), (int(*)(int))toupper);
+    for(int i = 0; i < c.length(); i++)
+    {
+        a[c[i] - 'A']++;
+    }
+
+    for(int i = 0; i < 26; i++)
+    {
+        if(max < a[i])
+        {
+            max = a[i];
+            cnt = 0;
+            target = i;
+        }
+        if(max == a[i]){
+            cnt++;
+        }
+    }
+    if(cnt > 1){
+        cout << "?";
+    }
+    else{
+        cout << (char)(target+'A');
+    }
+    return 0;
+}
