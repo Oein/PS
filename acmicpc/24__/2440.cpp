0fd4057658af9e792ba2828b88509fc39db0f0a5
Oein <oein0219@gmail.com>
2023-09-23 08:35:12 +0900

    solve: acmicpc / 2440.cpp

---
+++ acmicpc/24__/2440.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n = 0;
+    cin >> n;
+    for(int i = n;i > 0;i--){
+        for(int j = 0;j < i;j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
