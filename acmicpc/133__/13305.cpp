caeb10d6ca12121506814ffbed7bec038d4290da
Oein <oein0219@gmail.com>
2023-10-03 19:14:43 +0900

    solve: acmicpc / 13305.cpp

--- acmicpc/133__/13305.cpp
+++ acmicpc/133__/13305.cpp
@@ -2,37 +2,32 @@
 #include <vector>
 #include <climits>
 using namespace std;
-#define ull unsigned long long
 int dist[100005];
 int distSum[100005];
-ull dp[100005];
 int oilPrice[100005];
 int n;
-int distByIndex(int a, int b) {
-    if(a > b) swap(a, b);
-    return distSum[b] - distSum[a];
-}
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     cin >> n;
-    for(int i = 0;i < n - 1;i++) cin >> dist[i];
-    for(int i = 0;i < n;i++) {
+    for (int i = 0; i < n - 1; i++)
+        cin >> dist[i];
+
+    for (int i = 0; i < n; i++)
+    {
         cin >> oilPrice[i];
-        dp[i] = ULLONG_MAX;
         distSum[i + 1] = distSum[i] + dist[i];
     }
-    
-    // 끝 지점에서는 필요 없음
-    dp[n - 1] = 0;
-    
-    for(int i = n - 2; i >= 0;i--) {
-        // loop in all
-        for(int j = i + 1; j < n; j++) {
-            // i 에서 j 까지 사용될 연료를 지금 구매.
-            int dist_ = distByIndex(i,j);
-            ull price = oilPrice[i] * dist_;
-            dp[i] = min(dp[i], price + dp[j]);
-        }
+
+    int co = oilPrice[0];
+    unsigned long long res = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (co > oilPrice[i])
+            co = oilPrice[i];
+        res += (unsigned long long)co * (unsigned long long)dist[i];
     }
-    
-    cout << dp[0];
+
+    cout << res;
 }
