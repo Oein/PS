1dc4d86c54f05cfe3e0cfc72569f175b22d2fd8e
Oein <oein0219@gmail.com>
2023-10-03 19:03:34 +0900

    solve: acmicpc / 11399.cpp

---
+++ acmicpc/113__/11399.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+struct Pos {
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> k(n);
+    for (int i = 0; i < n; i++) cin >> k[i];
+    sort(k.begin(), k.end());
+    int sum = 0;
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        sum += k[i];
+        ans += sum;
+    }
+    cout << ans;
+}
