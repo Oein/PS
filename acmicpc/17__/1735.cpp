00c2259da74c7c6a92abf266ba135b28e3d5c88b
Oein <oein0219@gmail.com>
2023-10-05 16:45:47 +0900

    solve: acmicpc / 1735.cpp

---
+++ acmicpc/17__/1735.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long childa, moma, childb, momb;
+    cin >> childa >> moma >> childb >> momb;
+    long long newmom = moma * momb / gcd(moma, momb);
+    long long newchild = childa * (newmom / moma) + childb * (newmom / momb);
+    long long mcgcd = gcd(newmom, newchild);
+    cout << newchild / mcgcd << " " << newmom / mcgcd;
+}
