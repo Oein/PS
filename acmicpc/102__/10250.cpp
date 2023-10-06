4f5610538868d887feffcb3d9a9903b95a64d05d
Oein <oein0219@gmail.com>
2023-10-07 00:14:31 +0900

    solve: acmicpc / 10250.cpp

---
+++ acmicpc/102__/10250.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+void rmain()
+{
+    int h, w, n;
+    cin >> h >> w >> n;
+    int x = (n - 1) / h + 1;
+    int y = (n - 1) % h + 1;
+    cout << y << (x < 10 ? "0" : "") << x << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        rmain();
+}
