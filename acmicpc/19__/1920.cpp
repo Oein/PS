eefb0c2e563a89e25cea84492195dd0acedf00a8
Oein <oein0219@gmail.com>
2023-10-08 15:05:00 +0900

    solve: acmicpc / 1920.cpp

---
+++ acmicpc/19__/1920.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    set<int> x;
+    cin >> n;
+    while (n--)
+    {
+        int y;
+        cin >> y;
+        x.insert(y);
+    }
+    cin >> n;
+    while (n--)
+    {
+        int y;
+        cin >> y;
+        if (x.find(y) == x.end())
+            cout << "0\n";
+        else
+            cout << "1\n";
+    }
+}
