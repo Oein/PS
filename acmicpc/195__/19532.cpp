cf2e4fd6824deb51288e8f412b9573bd63a81a07
Oein <oein0219@gmail.com>
2023-10-11 21:12:31 +0900

    solve: acmicpc / 19532.cpp

---
+++ acmicpc/195__/19532.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    for (int x = -999; x <= 999; x++)
+        for (int y = -999; y <= 999; y++)
+        {
+            int a_ = x * a + b * y;
+            int b_ = x * d + e * y;
+            if (a_ == c && b_ == f)
+            {
+                cout << x << " " << y;
+                return 0;
+            }
+        }
+}
