f738f6907c75165e42c0fca018914d649b5c58fa
Oein <oein0219@gmail.com>
2023-10-11 21:26:32 +0900

    solve: acmicpc / 13909.cpp

---
+++ acmicpc/139__/13909.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, ans = 0;
+    cin >> n;
+    // 열려 있을려면 제곱수여야 한다.
+    for (int i = 1; i * i <= n; i++)
+        ans++;
+    cout << ans;
+}
