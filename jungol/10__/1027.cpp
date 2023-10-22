394d8c48c782f73430a0dce4b1e1f63c55fd9c37
Oein <oein0219@gmail.com>
2023-10-22 17:15:21 +0900

    solve: jungol / 1027.cpp

--- jungol/10__/1027.cpp
+++ jungol/10__/1027.cpp
@@ -1,43 +1,49 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-
-typedef long long ll;
-typedef pair<ll , ll> pii;
 
 int n;
 
-bool isOk(string num){
-    int numSize = num.size();
-    if(numSize < 2) return 1;
-    for(int i = 1;i <= numSize / 2;i++){
-        //                           두번 왼쪽 에서     i개
-        string leftStr = num.substr(numSize - i - i , i);
-        string rightStr = num.substr(numSize - i , i);
-        if(leftStr == rightStr) return 0;
+bool isOk(string s)
+{
+    int sl = s.size();
+    if (sl < 2)
+        return true;
+
+    for (int cds = 1; cds <= sl / 2; cds++)
+    {
+        string rst = s.substr(sl - cds, cds);
+        string lst = s.substr(sl - cds - cds, cds);
+        if (lst == rst)
+            return false;
     }
-    return 1;
+
+    return true;
 }
 
-// ull 써도 너무 커서 string
-void dfs(int digi , string num){
-    if(digi == n){
-        cout << num;
+void dfs(int dig, string s)
+{
+    if (dig == n)
+    {
+        cout << s;
         exit(0);
     }
 
-    for(int i = 1;i <= 3;i++){
-        string ist = to_string(i);
-        string newStr = num + ist;
-        if(!isOk(num)) continue;
+    for (int i = 1; i <= 3; i++)
+    {
+        string xyy = to_string(i);
+        string ns = s + xyy;
+        if (!isOk(ns))
+            continue;
 
-        dfs(digi + 1 , newStr);
+        dfs(dig + 1, ns);
     }
 }
 
-int main(){
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n;
-    dfs(1 , "1");
-    dfs(1 , "2");
-    dfs(1 , "3");
+    dfs(1, "1");
 }
