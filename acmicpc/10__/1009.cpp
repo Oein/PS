9b0412681368add8fa31dd591ccfab52ac9b497f
Oein <oein0219@gmail.com>
2023-11-04 19:20:40 +0900

    solve: acmicpc / 1009.cpp

---
+++ acmicpc/10__/1009.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+void rmain()
+{
+    int a, b;
+    cin >> a >> b;
+    int ans = 1;
+    while (b--)
+    {
+        ans *= a;
+        ans %= 10;
+    }
+    if (ans == 0)
+        ans = 10;
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        rmain();
+    }
+}
