7511ae446e2eed2e99b997b9b44723ec96a2c6a8
Oein <oein0219@gmail.com>
2023-11-05 12:00:39 +0900

    solve: acmicpc / 16430.cpp

---
+++ acmicpc/164__/16430.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << b - a << " " << b;
+}
