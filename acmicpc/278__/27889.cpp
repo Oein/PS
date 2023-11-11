09066c8bf56e4cdc42a25bdbe32048553d3b5492
Oein <oein0219@gmail.com>
2023-11-12 07:59:04 +0900

    solve: acmicpc / 27889.cpp

---
+++ acmicpc/278__/27889.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    char a;
+    cin >> a;
+    switch (a)
+    {
+    case 'N':
+        cout << "North London Collegiate School";
+        break;
+    case 'B':
+        cout << "Branksome Hall Asia";
+        break;
+    case 'K':
+        cout << "Korea International School";
+        break;
+    case 'S':
+        cout << "St. Johnsbury Academy";
+        break;
+    default:
+        break;
+    }
+}
