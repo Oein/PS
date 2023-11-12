dd25563563f3ed7b2f9885922008bd0fa485a053
Oein <oein0219@gmail.com>
2023-11-12 15:29:07 +0900

    solve: jungol / 2587.cpp

--- jungol/25__/2587.cpp
+++ jungol/25__/2587.cpp
@@ -33,16 +33,59 @@ struct Pos
     int y;
 };
 
+int n;
+
+const int MAXN = 500000;
+vector<pair<int, int> > arr; // pair<실력, idx>
+int tree[MAXN] = {
+    0,
+};
+
+int sum(int i)
+{
+    int ans = 0;
+    while (i > 0)
+    {
+        ans += tree[i];
+        i -= (i & -i);
+    }
+    return ans;
+}
+
+void update(int i, int num)
+{
+    while (i <= n)
+    {
+        tree[i] += num;
+        i += (i & -i);
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    int n;
     cin >> n;
-    vector<int> v(n);
-
     for (int i = 0; i < n; i++)
-        cin >> v[i];
+    {
+        int x;
+        cin >> x;
+        arr.push_back({x, i});
+    }
+
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+    {
+        arr[i].first = i + 1;
+        swap(arr[i].first, arr[i].second);
+    }
+
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 - sum(arr[i].second - 1) << '\n';
+        update(arr[i].second, 1);
+    }
 }
