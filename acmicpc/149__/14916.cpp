39b5325431fe47c1c7f660c518fcbb157049a439
Oein <oein0219@gmail.com>
2023-11-17 23:01:22 +0900

    solve: acmicpc / 14916.cpp

---
+++ acmicpc/149__/14916.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    while (true)
+    {
+        if (n % 5 == 0)
+        {
+            ans += n / 5;
+            break;
+        }
+        else
+        {
+            n -= 2;
+            ans++;
+        }
+
+        if (n < 0)
+        {
+            break;
+        }
+    }
+
+    if (n < 0)
+        ans = -1;
+    cout << ans;
+}
