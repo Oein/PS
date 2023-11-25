b0966d4849415f6c3e2395db38fc03bed27c84d7
Oein <oein0219@gmail.com>
2023-11-25 22:42:23 +0900

    solve: acmicpc / 2467.cpp

---
+++ acmicpc/24__/2467.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &c : v)
+        cin >> c;
+
+    sort(v.begin(), v.end());
+
+    int i = 0, j = n - 1;
+    int min_diff = INT_MAX;
+    int p1 = -1, p2 = -1;
+
+    while (i < j)
+    {
+        int sum = v[i] + v[j];
+        if (min_diff > abs(sum))
+        {
+            min_diff = abs(sum); // abs Math에 있더라
+            p1 = v[i], p2 = v[j];
+        }
+
+        if (sum == 0)
+            break;
+        else if (sum > 0)
+            --j;
+        else
+            ++i;
+    }
+
+    cout << p1 << ' ' << p2 << '\n';
+
+    return 0;
+}
