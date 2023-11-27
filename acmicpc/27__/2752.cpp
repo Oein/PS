59e2c3a28967535fda6937841b9e0f317bece562
Oein <oein0219@gmail.com>
2023-11-27 22:37:44 +0900

    solve: acmicpc / 2752.cpp

---
+++ acmicpc/27__/2752.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> a;
+    for (int i = 0; i < 3; i++)
+    {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    sort(a.begin(), a.end());
+    for (auto x : a)
+        cout << x << " ";
+}
