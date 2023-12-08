00b8a9836105e3455214d8c94d5e6df107f8a83f
Oein <oein0219@gmail.com>
2023-12-08 22:32:27 +0900

    solve: acmicpc / 24883.cpp

---
+++ acmicpc/248__/24883.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    char c;
+    cin >> c;
+    if (c == 'n' || c == 'N')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+}
