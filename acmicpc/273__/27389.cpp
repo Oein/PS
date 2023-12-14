7e73b51d841b4e0f86e01dcac8a29533b3d8c6f1
Oein <oein0219@gmail.com>
2023-12-14 21:47:04 +0900

    solve: acmicpc / 27389.cpp

---
+++ acmicpc/273__/27389.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    double N;
+    cin >> N;
+
+    double div = N / 4;
+    double l = div - (int)div;
+    if ((int)(l * 100) % 10)
+        printf("%.02f", N / 4);
+    else
+        printf("%.01f", N / 4);
+}
