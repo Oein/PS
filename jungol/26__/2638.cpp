5bb4c600a098c935400165f2a2065b586334ac8a
Oein <oein0219@gmail.com>
2024-01-07 17:19:56 +0900

    solve: jungol / 2638.cpp

---
+++ jungol/26__/2638.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+    int x;
+    int y;
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    int loc = 0;
+    cin >> n;
+    vector<int> resarr(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        while (resarr[loc] != 0)
+        {
+            loc++;
+            loc %= n;
+            // cout << "loac2: " << loc << "  data:" << resarr[loc] << endl;
+        }
+        int t;
+        cin >> t;
+        resarr[loc] = t;
+        loc += t;
+        loc %= n;
+    }
+
+    cout << n << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << resarr[i] << " ";
+    }
+}
