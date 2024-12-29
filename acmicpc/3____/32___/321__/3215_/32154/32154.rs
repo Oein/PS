949a0836b971662bd85f9849d17e8f530925e49b
Oein <oein0219@gmail.com>
2024-12-29 16:33:26 +0900

    solve: acmicpc / 32154.rs

---
+++ acmicpc/3____/32___/321__/3215_/32154/32154.rs
@@ -0,0 +1,192 @@
+use std::io::{stdin, Read};
+
+struct RustIO {
+    buffer: Vec<u8>,
+    cursor: usize,
+}
+
+#[allow(dead_code)]
+impl RustIO {
+    fn new() -> Self {
+        let mut buffer = Vec::new();
+        stdin().lock().read_to_end(&mut buffer).unwrap();
+
+        let mut buffer = buffer.clone();
+
+        // add \n to buffer for index out of bound by user input doesn't end with \n
+        buffer.push(b'\n');
+        Self { buffer, cursor: 0 }
+    }
+
+    fn consume_whitespace(&mut self) {
+        while self.buffer[self.cursor].is_ascii_whitespace() {
+            self.cursor += 1;
+        }
+    }
+
+    fn read_i32(&mut self) -> i32 {
+        self.consume_whitespace();
+
+        let is_negative = self.buffer[self.cursor] == b'-';
+        if is_negative {
+            self.cursor += 1;
+        }
+
+        let mut result = 0;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            result = result * 10 + (self.buffer[self.cursor] - b'0') as i32;
+            self.cursor += 1;
+        }
+
+        if is_negative {
+            result = -result;
+        }
+
+        result
+    }
+
+    fn read_i64(&mut self) -> i64 {
+        self.consume_whitespace();
+
+        let is_negative = self.buffer[self.cursor] == b'-';
+        if is_negative {
+            self.cursor += 1;
+        }
+
+        let mut result = 0;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            result = result * 10 + (self.buffer[self.cursor] - b'0') as i64;
+            self.cursor += 1;
+        }
+
+        if is_negative {
+            result = -result;
+        }
+
+        result
+    }
+
+    fn read_f32(&mut self) -> f32 {
+        self.consume_whitespace();
+
+        let is_negative = self.buffer[self.cursor] == b'-';
+        if is_negative {
+            self.cursor += 1;
+        }
+
+        let mut result = 0.0;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            if self.buffer[self.cursor] == b'.' {
+                self.cursor += 1;
+                let mut decimal = 0.1;
+                while !self.buffer[self.cursor].is_ascii_whitespace() {
+                    result += (self.buffer[self.cursor] - b'0') as f32 * decimal;
+                    decimal /= 10.0;
+                    self.cursor += 1;
+                }
+                break;
+            }
+            result = result * 10.0 + (self.buffer[self.cursor] - b'0') as f32;
+            self.cursor += 1;
+        }
+
+        if is_negative {
+            result = -result;
+        }
+
+        result
+    }
+
+    fn read_f64(&mut self) -> f64 {
+        self.consume_whitespace();
+
+        let is_negative = self.buffer[self.cursor] == b'-';
+        if is_negative {
+            self.cursor += 1;
+        }
+
+        let mut result = 0.0;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            if self.buffer[self.cursor] == b'.' {
+                self.cursor += 1;
+                let mut decimal = 0.1;
+                while !self.buffer[self.cursor].is_ascii_whitespace() {
+                    result += (self.buffer[self.cursor] - b'0') as f64 * decimal;
+                    decimal /= 10.0;
+                    self.cursor += 1;
+                }
+                break;
+            }
+            result = result * 10.0 + (self.buffer[self.cursor] - b'0') as f64;
+            self.cursor += 1;
+        }
+
+        if is_negative {
+            result = -result;
+        }
+
+        result
+    }
+
+    fn read_words(&mut self) -> String {
+        self.consume_whitespace();
+
+        let start = self.cursor;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            self.cursor += 1;
+        }
+
+        String::from_utf8(self.buffer[start..self.cursor].to_vec()).unwrap()
+    }
+
+    fn read_words_vecu8(&mut self) -> Vec<u8> {
+        self.consume_whitespace();
+
+        let start = self.cursor;
+        while !self.buffer[self.cursor].is_ascii_whitespace() {
+            self.cursor += 1;
+        }
+
+        let result = self.buffer[start..self.cursor].to_vec();
+        self.cursor += 1;
+        result
+    }
+
+    fn read_line(&mut self) -> String {
+        self.consume_whitespace();
+
+        let start = self.cursor;
+        while self.buffer[self.cursor] != b'\n' {
+            self.cursor += 1;
+        }
+
+        self.cursor += 1;
+        String::from_utf8(self.buffer[start..self.cursor].to_vec()).unwrap()
+    }
+}
+
+// ==================== End of IO lib ====================
+
+fn main() {
+    let mut io = RustIO::new();
+
+    let a = io.read_i32() as usize;
+
+    let d = [
+        vec![1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 9, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 9, 12, 13],
+        vec![1, 2, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 3, 5, 6, 7, 8, 12, 13],
+        vec![1, 2, 3, 6, 7, 8, 12, 13],
+    ];
+
+    println!("{}", d[a as usize - 1].len());
+    for dx in d[a as usize - 1].iter() {
+        print!("{} ", (b'A' + dx - 1) as char);
+    }
+}
---
+++ acmicpc/3____/32___/321__/3215_/32154/README.md
@@ -0,0 +1,4 @@
+# 32154
+
+Problem from [acmicpc](https://www.acmicpc.net/problem/32154)
+
