e6f6b51d1cbc0847e486da1d7199b587e97ba64f
Oein <oein0219@gmail.com>
2025-06-02 16:10:49 +0900

    solve: acmicpc / 9649.cpp

---
+++ acmicpc/9___/96__/964_/9649/9649.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int a, b, c;
+  while (cin >> a >> b >> c)
+  {
+    int pr = c - b;
+    int ch = a * c - pr * a;
+    int gc = gcd(ch, pr);
+    ch /= gc;
+    pr /= gc;
+    cout << ch << "/" << pr << '\n';
+  }
+}
---
+++ acmicpc/9___/96__/964_/9649/README.md
@@ -0,0 +1,4 @@
+# 9649
+
+Problem from [acmicpc](https://www.acmicpc.net/problem/9649)
+
