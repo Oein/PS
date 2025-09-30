74640452d1e94cbbf602ba2ba8978134ed96ca04
Oein <oein@oein.kr>
2025-09-30 21:17:32 +0900

    solve: acmicpc / 1024.cpp

--- acmicpc/1___/10__/102_/1024/1024.cpp
+++ acmicpc/1___/10__/102_/1024/1024.cpp
@@ -27,13 +27,45 @@ using namespace std;
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 
-struct Pos {
+struct Pos
+{
   int x;
   int y;
 };
 
-int main() {
+int main()
+{
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
+  int n, l;
+  cin >> n >> l;
+
+  // 개수 X
+  // Starter S
+  // sum = X * (X + 1) / 2 + S * X
+  // sum = X * (X + 1 + 2S) / 2
+  // 2 * sum = X^2 + X + 2SX
+  // 2SX = 2 * sum - X^2 - X
+  // S = (2 * sum - X^2 - X) / (2 * X)
+
+  for (int x = l; x <= 100; x++)
+  {
+    int sa = (2 * n - x * x - x);
+    int sb = (2 * x);
+    if (sa % sb != 0)
+      continue;
+    int s = sa / sb + 1;
+    if (s < 0)
+      continue;
+    for (int i = 0; i < x; i++)
+    {
+      cout << s + i << (i == x - 1 ? "\n" : " ");
+    }
+    return 0;
+  }
+
+  cout << -1;
+  return 0;
 }
