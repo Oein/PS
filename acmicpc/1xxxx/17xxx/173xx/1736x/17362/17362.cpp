0f36463f2fff4de400cdc642f6958aabdd447017
Oein <oein@oein.kr>
2025-12-22 22:35:53 +0900

    solve: acmicpc / 17362.cpp

---
+++ acmicpc/1xxxx/17xxx/173xx/1736x/17362/17362.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  cin >> n;
+  n--;
+  n %= 8;
+  if (n == 5)
+    cout << 4;
+  else if (n == 6)
+    cout << 3;
+  else if (n == 7)
+    cout << 2;
+  else
+    cout << n + 1;
+}
---
+++ acmicpc/1xxxx/17xxx/173xx/1736x/17362/README.md
@@ -0,0 +1,2 @@
+# Problem 17362
+[Problem Link](https://www.acmicpc.net/problem/17362)
