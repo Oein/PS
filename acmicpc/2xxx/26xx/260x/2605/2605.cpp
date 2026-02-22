47468549fa765d60392cf962d792e3788c4d809f
Oein <oein@oein.kr>
2026-02-22 22:14:21 +0900

    solve: acmicpc / 2605.cpp

---
+++ acmicpc/2xxx/26xx/260x/2605/2605.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  cin >> n;
+
+  vector<int> ans;
+
+  for (int i = 1; i <= n; i++)
+  {
+    int k;
+    cin >> k;
+    ans.insert(ans.begin() + ans.size() - k, i);
+  }
+
+  for (int i = 0; i < n; i++)
+    cout << ans[i] << ' ';
+
+  return 0;
+}
---
+++ acmicpc/2xxx/26xx/260x/2605/README.md
@@ -0,0 +1,2 @@
+# Problem 2605
+[Problem Link](https://www.acmicpc.net/problem/2605)
