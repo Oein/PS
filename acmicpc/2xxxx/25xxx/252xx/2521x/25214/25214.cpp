8297f3f7472c3a25a6be5a8aaeb20433877779d4
Oein <oein@oein.kr>
2026-02-22 22:27:11 +0900

    solve: acmicpc / 25214.cpp

---
+++ acmicpc/2xxxx/25xxx/252xx/2521x/25214/25214.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int N;
+  cin >> N;
+
+  ll mn = LLONG_MAX;
+  ll mx = 0;
+
+  for (int i = 0; i < N; i++)
+  {
+    ll x;
+    cin >> x;
+
+    mn = min(mn, x);
+    mx = max(mx, x - mn);
+    cout << mx << ' ';
+  }
+}
---
+++ acmicpc/2xxxx/25xxx/252xx/2521x/25214/README.md
@@ -0,0 +1,2 @@
+# Problem 25214
+[Problem Link](https://www.acmicpc.net/problem/25214)
