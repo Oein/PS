2afc58327807a0a600761da55aa57ffffc98740d
Oein <oein@oein.kr>
2026-02-22 21:42:26 +0900

    solve: acmicpc / 16693.cpp

---
+++ acmicpc/1xxxx/16xxx/166xx/1669x/16693/16693.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  double A1, P1, R1, P2;
+  cin >> A1 >> P1;
+  cin >> R1 >> P2;
+
+  cout << ((M_PI * R1 * R1) / P2 > (A1 / P1) ? "Whole pizza" : "Slice of pizza");
+
+  return 0;
+}
