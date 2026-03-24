d93210565ed8ef4f787badf76d6367df7f36d7a0
Oein <oein@oein.kr>
2026-03-25 07:52:42 +0900

    solve: acmicpc / 11256.cpp

---
+++ acmicpc/1xxxx/11xxx/112xx/1125x/11256/11256.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+#define zx3f 1061109567
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+struct Pos
+{
+  int x;
+  int y;
+};
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int j, n;
+    cin >> j >> n;
+    vector<int> a;
+    while (n--)
+    {
+      int q, w;
+      cin >> q >> w;
+      a.push_back(q * w);
+    }
+    sort(a.begin(), a.end(), greater<int>());
+    int ans = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+      if (j <= 0)
+        break;
+      j -= a[i];
+      ans++;
+    }
+    cout << ans << "\n";
+  }
+}
---
+++ acmicpc/1xxxx/11xxx/112xx/1125x/11256/README.md
@@ -0,0 +1,2 @@
+# Problem 11256
+[Problem Link](https://www.acmicpc.net/problem/11256)
